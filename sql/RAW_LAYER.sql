b05f33867d7efd9a497945ff9e475d42b4ad2aee
AjitMankar <aj2mankar@gmail.com>
2026-03-11 17:16:16 -0400

    Create RAW_LAYER.sql

---
+++ sql/RAW_LAYER.sql
@@ -0,0 +1,51 @@
+create or replace table raw_data (raw_file variant, filename  varchar(100));
+
+create or replace storage integration s3_int 
+type= external_stage
+storage_provider=s3
+enabled=True
+storage_aws_role_arn='arn:aws:iam::068606397959:role/Snowflake_access' arn:aws:iam::068606397959:role/Snowflake_access
+storage_allowed_locations=('s3://snowflake-bootcamp-2025ajit/Cricket Analytics/')  arn:aws:iam::068606397959:role/Snowflake_access
+
+describe storage integration s3_int  --arn:aws:iam::761587731547:user/fuqb1000-s PBC03780_SFCRole=5_RlI62K3MjwWxm/NdmtC9+py/QeM=
+create or replace storage integration s3_int 
+type= external_stage
+storage_provider=s3
+enabled=True
+storage_aws_role_arn='arn:aws:iam::068606397959:role/Snowflake_access'
+storage_allowed_locations=('s3://snowflake-bootcamp-2025ajit/product/','s3://snowflake-bootcamp-2025ajit/Orders/','s3://snowflake-bootcamp-2025ajit/Cricket Analytics/')
+--storage_blocked_locations 
+
+create or replace stage s3_ext_stage
+storage_integration=s3_int
+FILE_FORMAT = (TYPE = JSON)
+url = 's3://snowflake-bootcamp-2025ajit/Cricket Analytics/'
+
+show stages;
+select * from @s3_ext_stage
+create or replace pipe s3_pipe auto_ingest=True as 
+COPY INTO raw_data
+  from (select $1 , metadata$filename as filename from @s3_ext_stage)
+on_error=continue
+
+select  system$pipe_status('s3_pipe')
+select $1 , metadata$filename as filename from @s3_ext_stage
+
+show pipes 
+
+select * from RAW.raw_data
+
+create or replace stream stream_raw_data on table RAW.raw_data
+append_only=TRUE
+
+show streams
+stream_raw_data
+select * from stream_raw_data
+
+create or replace table raw.default_powerplays 
+(match_type varchar(10),
+start_over int ,
+end_over int 
+);
+insert into raw.default_powerplays values ('ODI',1,10),('T20',1,6)
+select * from raw.default_powerplays;
